348b92eef76e454e38a663068639df0659951e06
zxliang98 <1287520001@qq.com>
2020-12-16 14:41:06 +0800

    debug

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/home/Home.vue'
 import Test from '../views/test/Test.vue'
-import Pub from './../views/publish/publish.vue'
+import Pub from './../views/publish/Publish.vue'
 
 Vue.use(VueRouter)
 
